12d352034abd7fbb1d42258ee41e92ff29446eae
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-13 21:18:11 -0500

    Update deps, move devdeps and upgrade to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
+/* eslint-env node */
+
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const webpack = require("webpack");
+const root = __dirname;
 
-module.exports = {
-  entry: {
-    dungeon: "./src/index.js",
-    "dungeon.min": "./src/index.js"
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    library: "Dungeon",
-    libraryTarget: "umd"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
+module.exports = function(env, argv) {
+  const isDev = argv.mode === "development";
+
+  return {
+    context: path.join(root, "src"),
+    entry: {
+      dungeon: "./index.js",
+      "dungeon.min": "./index.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.join(root, "dist"),
+      library: "Dungeon",
+      libraryTarget: "umd",
+      libraryExport: "default"
+    },
+    optimization: {
+      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"]
+            }
           }
         }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true,
-      sourceMap: "none"
-    })
-  ]
+      ]
+    },
+    devtool: isDev ? "eval-source-map" : "source-map"
+  };
 };
