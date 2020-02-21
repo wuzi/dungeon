11a86eee285d1605cde2e7480f37c1bba36c3938
Michael Hadley <michaelwesthadley@gmail.com>
2020-02-21 07:16:23 -0600

    Convert tests to TS

--- src/dungeon.test.ts
+++ src/dungeon.test.ts
@@ -18,7 +18,7 @@ describe("Dungeon constructor", () => {
     const d = new Dungeon({
       width: 20,
       rooms: {
-        width: { min: 2, onlyOdd: true },
+        width: { min: 3, onlyOdd: true },
         maxArea: 200
       }
     });
@@ -32,37 +32,38 @@ describe("Dungeon constructor", () => {
     expect(d.height).toBe(50);
   });
 
-  test("should ignore maxArea if too small", () => {
-    const d = new Dungeon({
-      rooms: {
-        width: { min: 10, max: 20 },
-        height: { min: 10, max: 20 },
-        maxArea: 20
-      }
-    });
-    expect(d.roomConfig.maxArea).toBe(100);
+  test("should throw error if maxArea too small", () => {
+    expect(() => {
+      const d = new Dungeon({
+        rooms: {
+          width: { min: 10, max: 20 },
+          height: { min: 10, max: 20 },
+          maxArea: 20
+        }
+      });
+    }).toThrowError(/exceeds the given maxArea/);
   });
 
   test("should not allow rooms smaller than 3 x 3", () => {
-    const d = new Dungeon({
-      rooms: {
-        width: { min: 0, max: 20 },
-        height: { min: -2, max: 20 }
-      }
-    });
-    expect(d.roomConfig.width.min).toBe(3);
-    expect(d.roomConfig.height.min).toBe(3);
+    expect(() => {
+      const d = new Dungeon({
+        rooms: {
+          width: { min: 0, max: 20 },
+          height: { min: -2, max: 20 }
+        }
+      });
+    }).toThrowError(/width and height must be >= 3/);
   });
 
   test("should not allow room size min less than max", () => {
-    const d = new Dungeon({
-      rooms: {
-        width: { min: 5, max: 4 },
-        height: { min: 2, max: 1 }
-      }
-    });
-    expect(d.roomConfig.width.max).toBe(5);
-    expect(d.roomConfig.height.max).toBe(3);
+    expect(() => {
+      const d = new Dungeon({
+        rooms: {
+          width: { min: 5, max: 4 },
+          height: { min: 3, max: 1 }
+        }
+      });
+    }).toThrowError(/width and height max must be >= min/);
   });
 });
 
@@ -78,10 +79,10 @@ describe("A dungeon", () => {
       }
     });
     for (const room of d.rooms) {
-      expect(room.tiles[0][0]).not.toBe(TILES.DOOR);
-      expect(room.tiles[room.height - 1][0]).not.toBe(TILES.DOOR);
-      expect(room.tiles[0][room.width - 1]).not.toBe(TILES.DOOR);
-      expect(room.tiles[room.height - 1][room.width - 1]).not.toBe(TILES.DOOR);
+      expect(room.getTileAt(0, 0)).not.toBe(TILES.DOOR);
+      expect(room.getTileAt(0, room.height - 1)).not.toBe(TILES.DOOR);
+      expect(room.getTileAt(room.width - 1, 0)).not.toBe(TILES.DOOR);
+      expect(room.getTileAt(room.width - 1, room.height - 1)).not.toBe(TILES.DOOR);
     }
   });
 });
--- src/random.test.ts
+++ src/random.test.ts
@@ -2,7 +2,7 @@ import Random from "./random";
 
 describe("randomInteger", () => {
   test("should return a number between min and max", () => {
-    const r = new Random(0);
+    const r = new Random("seed");
     Array(100)
       .map(() => r.randomInteger(3, 5))
       .forEach(r => {
@@ -12,7 +12,7 @@ describe("randomInteger", () => {
   });
 
   test("should return a number between min and max when min > max", () => {
-    const r = new Random(0);
+    const r = new Random("seed");
     Array(100)
       .map(() => r.randomInteger(5, 3))
       .forEach(r => {
@@ -21,37 +21,37 @@ describe("randomInteger", () => {
       });
   });
 
-  test("should return an even number when isEven option is used", () => {
-    const r = new Random(0);
+  test("should return an even number when onlyEven option is used", () => {
+    const r = new Random("seed");
     Array(100)
-      .map(() => r.randomInteger(2, 30, { isEven: true }))
+      .map(() => r.randomInteger(2, 30, { onlyEven: true }))
       .forEach(r => {
         expect(r % 2).toBe(0);
       });
   });
 
-  test("should return an even number when isEven option is used and parameters are odd", () => {
-    const r = new Random(0);
+  test("should return an even number when onlyEven option is used and parameters are odd", () => {
+    const r = new Random("seed");
     Array(100)
-      .map(() => r.randomInteger(3, 31, { isEven: true }))
+      .map(() => r.randomInteger(3, 31, { onlyEven: true }))
       .forEach(r => {
         expect(r % 2).toBe(0);
       });
   });
 
-  test("should return an odd number when isOdd option is used", () => {
-    const r = new Random(0);
+  test("should return an odd number when onlyOdd option is used", () => {
+    const r = new Random("seed");
     Array(100)
-      .map(() => r.randomInteger(3, 31, { isOdd: true }))
+      .map(() => r.randomInteger(3, 31, { onlyOdd: true }))
       .forEach(r => {
         expect(r % 2).toBe(1);
       });
   });
 
-  test("should return an odd number when isOdd option is used and parameters are even", () => {
-    const r = new Random(0);
+  test("should return an odd number when onlyOdd option is used and parameters are even", () => {
+    const r = new Random("seed");
     Array(100)
-      .map(() => r.randomInteger(2, 30, { isOdd: true }))
+      .map(() => r.randomInteger(2, 30, { onlyOdd: true }))
       .forEach(r => {
         expect(r % 2).toBe(1);
       });
